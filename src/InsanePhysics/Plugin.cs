a50b352e2e448ee4546eebe85c2fea4180ba4c09
Ryocery <youri.haentjens@gmail.com>
2025-11-18 23:30:32 +0100

    Initial commit

---
+++ src/InsanePhysics/Plugin.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace InsanePhysics;
+
+[BepInAutoPlugin]
+public partial class Plugin : BaseUnityPlugin {
+    private static ManualLogSource Log { get; set; } = null!;
+
+    private void Awake() {
+        Log = Logger;
+        Log.LogInfo($"Plugin {Name} is loaded!");
+    }
+}
